da28d5db6a022f2f0ff67fff5acb7a80f4582953
saguirregaray1 <saguirregaray1@correo.um.edu.uy>
2025-11-05 17:27:12 -0300

    add profile page that shows personal info and manages pats

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as DashboardIcon } from './DashboardIcon'
 export { default as JobsIcon } from './JobsIcon'
+export { default as ProfileIcon } from './ProfileIcon'
 export { default as UsersIcon } from './UsersIcon'
